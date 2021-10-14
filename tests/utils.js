16858d6c0cdc69b2ac7db0d4001a605c45d98595
Evangelos Paterakis <evan@geopjr.dev>
2021-10-14 16:48:00 +0300

    fix(utils): use heredoc instead of literal

--- tests/utils.js
+++ tests/utils.js
@@ -4,7 +4,8 @@ import { write, writeFileSync } from 'fs';
 export function genCrystalExample(code, input) {
 	const mainStruct = code.match(/struct (?<struct>O_.+)\n/)?.groups?.struct;
 	const res = ['require "json"', `${code}`];
-	res.push(`\npp ${mainStruct}.from_json(%(${JSON.stringify(JSON.parse(input))}))`);
+	res.push(`input = <<-JSON\n${JSON.stringify(JSON.parse(input))}\nJSON`);
+	res.push(`pp ${mainStruct}.from_json(input)`);
 	return res.join('\n\n');
 }
 
