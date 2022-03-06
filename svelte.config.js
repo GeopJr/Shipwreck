1bd6df512a02805d031f58084fb855cb02c643f1
Evangelos Paterakis <evan@geopjr.dev>
2022-03-06 02:18:23 +0200

    fix: sveltekit breaking changes

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,7 @@ import staticAdapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: staticAdapter({ fallback: 'index.html' }),
-		target: '#svelte'
+		adapter: staticAdapter({ fallback: 'index.html' })
 	}
 };
 
